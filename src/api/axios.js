19b48e608ee58cf107f8ae653018596d4ae528f1
Felipe Oliveira <felipe.oliveira@ZBN.GROUP>
2024-12-26 15:06:55 -0300

    alterações

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001', // Ensure this is the correct server address and port
+  baseURL: 'http://localhost:5001', // Certifique-se de que este é o endereço e a porta corretos do servidor
   // ...existing code...
 });
 

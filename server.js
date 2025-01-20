e0b7ebdd3014d16007682687f9fc63fcf9092c44
fesao09 <feoliveira339@gmail.com>
2025-01-20 15:48:56 -0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5001;
-
-// ...existing code...
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
